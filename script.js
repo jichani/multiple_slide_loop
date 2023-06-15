0b1f85f3d9fbbae566f946c52933224d3fa957c8
jichani <jichan7571@naver.com>
2023-06-15 19:37:52 +0900

    무한루프구현완료

--- script.js
+++ script.js
@@ -28,6 +28,11 @@ function makeClone() {
     }
     updateWidth();
     setInitialPosition();
+
+    setTimeout(function () {
+        slides.classList.add('animated');
+    }, 100)
+
 }
 
 function updateWidth() {
@@ -44,3 +49,26 @@ function setInitialPosition() {
     slides.style.transform = `translateX(${initialTranslateValue}px)`
 }
 
+nextBtn.addEventListener('click', function () {
+    moveSlide(currentIdx + 1);
+})
+prevBtn.addEventListener('click', function () {
+    moveSlide(currentIdx - 1);
+})
+
+function moveSlide(num) {
+    slides.style.left = -num * (slideWidth + slideMargin) + 'px';
+    currentIdx = num;
+    console.log(currentIdx, slideCount);
+
+    if (currentIdx == slideCount || currentIdx == - slideCount) {
+        setTimeout(function () {
+            slides.classList.remove('animated');
+            slides.style.left = '0px';
+            currentIdx = 0;
+        }, 500);
+        setTimeout(function () {
+            slides.classList.add('animated');
+        }, 600)
+    }
+}
